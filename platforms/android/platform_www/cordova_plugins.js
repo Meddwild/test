e6ff9a0e03f8528f93e0fc5da96ab76f690361ec
meddwild <mitch.dewilde@ugent.be>
2018-02-17 12:21:46 +0100

    f

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,28 +1,28 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
-    "id": "cordova-plugin-googleplus.GooglePlus",
-    "file": "plugins/cordova-plugin-googleplus/www/GooglePlus.js",
-    "pluginId": "cordova-plugin-googleplus",
-    "clobbers": [
-      "window.plugins.googleplus"
-    ]
-  },
-  {
     "id": "cordova-plugin-mauron85-background-geolocation.backgroundGeolocation",
     "file": "plugins/cordova-plugin-mauron85-background-geolocation/www/backgroundGeolocation.js",
     "pluginId": "cordova-plugin-mauron85-background-geolocation",
     "clobbers": [
       "backgroundGeolocation"
     ]
+  },
+  {
+    "id": "cordova-plugin-googleplus.GooglePlus",
+    "file": "plugins/cordova-plugin-googleplus/www/GooglePlus.js",
+    "pluginId": "cordova-plugin-googleplus",
+    "clobbers": [
+      "window.plugins.googleplus"
+    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-googleplus": "5.1.1",
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-mauron85-background-geolocation": "2.3.3"
+  "cordova-plugin-mauron85-background-geolocation": "2.3.3",
+  "cordova-plugin-googleplus": "5.2.1"
 };
 // BOTTOM OF METADATA
 });
--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -1,36 +1,28 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-googleplus/www/GooglePlus.js",
-        "id": "cordova-plugin-googleplus.GooglePlus",
-        "pluginId": "cordova-plugin-googleplus",
-        "clobbers": [
-            "window.plugins.googleplus"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-googleplus/src/browser/GooglePlusProxy.js",
-        "id": "cordova-plugin-googleplus.GooglePlusProxy",
-        "pluginId": "cordova-plugin-googleplus",
-        "clobbers": [
-            "GooglePlus"
-        ]
-    },
-    {
         "file": "plugins/cordova-plugin-mauron85-background-geolocation/www/backgroundGeolocation.js",
         "id": "cordova-plugin-mauron85-background-geolocation.backgroundGeolocation",
         "pluginId": "cordova-plugin-mauron85-background-geolocation",
         "clobbers": [
             "backgroundGeolocation"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-googleplus/www/GooglePlus.js",
+        "id": "cordova-plugin-googleplus.GooglePlus",
+        "pluginId": "cordova-plugin-googleplus",
+        "clobbers": [
+            "window.plugins.googleplus"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-googleplus": "5.1.1",
     "cordova-plugin-whitelist": "1.3.3",
-    "cordova-plugin-mauron85-background-geolocation": "2.3.3"
+    "cordova-plugin-mauron85-background-geolocation": "2.3.3",
+    "cordova-plugin-googleplus": "5.2.1"
 }
 // BOTTOM OF METADATA
 });
