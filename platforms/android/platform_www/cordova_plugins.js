f79e2e5ef8bc430ae68a5e4b3d1d94fcb742896c
meddwild <mitch.dewilde@ugent.be>
2018-02-17 13:02:24 +0100

    k

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,19 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
-    "id": "cordova-plugin-mauron85-background-geolocation.backgroundGeolocation",
-    "file": "plugins/cordova-plugin-mauron85-background-geolocation/www/backgroundGeolocation.js",
-    "pluginId": "cordova-plugin-mauron85-background-geolocation",
-    "clobbers": [
-      "backgroundGeolocation"
-    ]
-  },
-  {
     "id": "cordova-plugin-googleplus.GooglePlus",
     "file": "plugins/cordova-plugin-googleplus/www/GooglePlus.js",
     "pluginId": "cordova-plugin-googleplus",
     "clobbers": [
       "window.plugins.googleplus"
+    ]
+  },
+  {
+    "id": "cordova-plugin-mauron85-background-geolocation.backgroundGeolocation",
+    "file": "plugins/cordova-plugin-mauron85-background-geolocation/www/backgroundGeolocation.js",
+    "pluginId": "cordova-plugin-mauron85-background-geolocation",
+    "clobbers": [
+      "backgroundGeolocation"
     ]
   }
 ];
@@ -21,8 +21,8 @@ module.exports.metadata =
 // TOP OF METADATA
 {
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-mauron85-background-geolocation": "2.3.3",
-  "cordova-plugin-googleplus": "5.2.1"
+  "cordova-plugin-googleplus": "5.2.1",
+  "cordova-plugin-mauron85-background-geolocation": "2.2.5"
 };
 // BOTTOM OF METADATA
 });
